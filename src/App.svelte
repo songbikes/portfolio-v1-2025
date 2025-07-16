88980a8489821b12850d2809d5fd40cbd27674ed
songbikes <song@asobi.tw>
2025-07-16 14:42:49 +1200

    clean up scss a bit

--- src/App.svelte
+++ src/App.svelte
@@ -3,22 +3,3 @@
 </script>
 
 <Portfolio />
-
-<!-- <style>
-  :global(body) {
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
-      'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
-      sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-  }
-
-  :global(*) {
-    box-sizing: border-box;
-  }
-
-  :global(html, body, #app) {
-    height: 100%;
-    overflow: hidden;
-  }
-</style> -->
