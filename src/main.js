3b76fac65056c20c48d13e15b8fe77c9e7cb103a
songbikes <song@asobi.tw>
2025-07-17 18:26:38 +1200

    import { inject } from '@vercel/analytics'; inject();

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import { mount } from "svelte";
 import "./app.css";
 import App from "./App.svelte";
+import { inject } from "@vercel/analytics";
+inject();
 
 const app = mount(App, {
   target: document.getElementById("app"),
